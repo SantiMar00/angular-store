1f2225e1d83503d5f1d99df185a36786f02decb4
Santiago Mariño <santimate30@hotmail.com>
2023-10-16 20:03:49 -0300

    product

---
+++ src/app/pages/products/product/product.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product!: Product
+  @Output() addToCartClick = new EventEmitter<Product>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.addToCartClick.emit(this.product)
+  }
+
+}
