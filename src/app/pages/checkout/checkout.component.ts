4f9ea3a1fdcdb2969dc5bfd806c3c4b090808b8c
Santiago Mariño <santimate30@hotmail.com>
2023-10-17 15:57:04 -0300

    pickup or delivery form

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,59 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/shared/services/data.service';
+import { tap } from 'rxjs/operators';
+import { Store } from 'src/app/shared/interfaces/store.interface';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
-export class CheckoutComponent {
-  
+export class CheckoutComponent implements OnInit {
   model = {
-    name:'',
-    store:'',
-    shippingAddress:'',
-    city:''
+    name: '',
+    store: '',
+    shippingAddress: '',
+    city: '',
+  };
+
+  isDelivery = false;
+
+  stores: Store[] = [];
+
+  constructor(private dataSvc: DataService) {}
+
+  ngOnInit(): void {
+    this.getStores();
   }
 
-  stores = [
-    {
-      "id": 1,
-      "name": "Park Row at Beekman St",
-      "address": "38 Park Row",
-      "city": "New York",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 2,
-      "name": "Store Alcalá",
-      "address": "Calle de Alcalá, 21",
-      "city": "Madrid",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 3,
-      "name": "Chambers and West Broadway",
-      "address": "125 Chambers Street",
-      "city": "New York",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 4,
-      "name": "Covent Garden - Russell St",
-      "address": "10 Russell Street",
-      "city": "London",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 5,
-      "name": "Monmouth St",
-      "address": "11 Monmouth Street",
-      "city": "London",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    }
-  ]
-
-  onPickupOrDelivery(value: Boolean): void {
-    console.log(value)
+  onPickupOrDelivery(value: boolean): void {
+    this.isDelivery = value;
   }
 
+  onSubmit(): void {
+    console.log('GOL');
+  }
+
+  private getStores(): void {
+    this.dataSvc
+      .getStores()
+      .pipe(tap((stores: Store[]) => (this.stores = stores)))
+      .subscribe();
+  }
 }
--- src/app/shared/services/shopping-cart.service.ts
+++ src/app/shared/services/shopping-cart.service.ts
@@ -1,52 +1,57 @@
-import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
-import { Product } from "src/app/pages/products/interfaces/product.interface";
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Product } from 'src/app/pages/products/interfaces/product.interface';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class ShoppingCartService {
-  
-  products: Product[] = []
+  products: Product[] = [];
 
-  private cartSubject = new Subject<Product[]>()
-  private totalSubject = new Subject<number>()
-  private quantitySubject = new Subject<number>()
-
+  private cartSubject = new BehaviorSubject<Product[]>([]);
+  private totalSubject = new BehaviorSubject<number>(0);
+  private quantitySubject = new BehaviorSubject<number>(0);
 
   get cartAction$(): Observable<Product[]> {
-    return this.cartSubject.asObservable()
+    return this.cartSubject.asObservable();
   }
 
   get totalAction$(): Observable<number> {
-    return this.totalSubject.asObservable()
+    return this.totalSubject.asObservable();
   }
 
   get quantityAction$(): Observable<number> {
-    return this.quantitySubject.asObservable()
+    return this.quantitySubject.asObservable();
   }
 
-  
   updateCart(product: Product): void {
-    this.addToCart(product)
-    this.quantityProducts()
-    this.calcTotal()
+    this.addToCart(product);
+    this.quantityProducts();
+    this.calcTotal();
   }
 
   private addToCart(product: Product): void {
-    this.products.push(product)
-    this.cartSubject.next(this.products)
+    const isProductInCart = this.products.find(({ id }) => id === product.id);
+
+    if (isProductInCart) {
+      isProductInCart.qty += 1;
+    } else {
+      this.products.push({ ...product, qty: 1 });
+    }
+
+    this.cartSubject.next(this.products);
   }
 
   private quantityProducts(): void {
-    const quantity = this.products.length
-    this.quantitySubject.next(quantity)
+    const quantity = this.products.reduce((acc, prod) => (acc += prod.qty), 0);
+    this.quantitySubject.next(quantity);
   }
 
   private calcTotal(): void {
-    const total = this.products.reduce((acc, prod) => acc += prod.price, 0)
-    this.totalSubject.next(total)
+    const total = this.products.reduce(
+      (acc, prod) => (acc += prod.price * prod.qty),
+      0
+    );
+    this.totalSubject.next(total);
   }
-
-
 }
